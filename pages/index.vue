05dae753a779d46d0f48212e39cd81af694926dc
chibi929 <ibihc929@gmail.com>
2022-05-04 22:19:59 +0900

    [Add] Add pages for Main.

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="index-page">
-    <h1>IndexPage</h1>
+    <div class="profile">
+      <img src="@/assets/images/chibi929.png" />
+      <p>chibi929</p>
+    </div>
+
+    <div class="links">
+      <a href="https://twitter.com/ibihc929">
+        <img src="@/assets/images/twitter_logo.png" alt="Twitter" />
+      </a>
+      <a href="https://www.facebook.com/chibi929">
+        <img src="@/assets/images/facebook_logo.png" alt="Facebook" />
+      </a>
+      <a href="https://github.com/chibi929">
+        <img src="@/assets/images/github_logo.png" alt="GitHub" />
+      </a>
+      <a href="https://qiita.com/chibi929">
+        <img src="@/assets/images/qiita_logo.png" alt="Qiita" />
+      </a>
+    </div>
   </div>
 </template>
 
@@ -21,10 +39,32 @@ export default class IndexPage extends Vue {
 }
 </script>
 
+<style lang="scss">
+html,
+body,
+#__nuxt,
+#__layout {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
 <style lang="scss" scoped>
 .index-page {
-  h1 {
-    color: $color_primary;
+  height: 100%;
+  background-color: #eeffee;
+  text-align: center;
+
+  .profile img {
+    width: 128px;
+  }
+
+  .links {
+    margin-top: 1rem;
+
+    img {
+      width: 32px;
+    }
   }
 }
 </style>
